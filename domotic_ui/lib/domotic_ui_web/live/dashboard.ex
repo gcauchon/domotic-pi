5acccb0e153f1e7c2600a80b7126bda9f9f29b27
Guillaume Cauchon <gcauchon@gmail.com>
2020-04-19 15:40:04 -0400

    Use Phoenix.PubSub to receive updates

--- domotic_ui/lib/domotic_ui_web/live/dashboard.ex
+++ domotic_ui/lib/domotic_ui_web/live/dashboard.ex
@@ -1,13 +1,35 @@
 defmodule DomoticUiWeb.Live.Dashboard do
   use Phoenix.LiveView
+
+  @temperature_threshold -15.0
  
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :temperature, 0)}
+    Phoenix.PubSub.subscribe(DomoticUi.PubSub, "watcher")
+
+    {:ok, assign(socket, :temperature, @temperature_threshold)}
   end
 
   def render(assigns) do
     ~L"""
-    Current temperature: <%= @temperature %> 
+    <button type="button" class="btn <%= temperature_level(@temperature) %>">
+      Temperature <span class="badge badge-light"><%= @temperature %>°C</span>
+    </button>
+
+    <span class="dashboard__tooltip">Target: <%= temperature_threshold() %>°C</span>
     """
+  end
+
+  def handle_info(%{timestamp: _timestamp, temperature: temperature}, socket) do
+    {:noreply, assign(socket, :temperature, temperature)}
+  end
+
+  defp temperature_threshold, do: @temperature_threshold
+
+  defp temperature_level(temperature) do
+    if temperature <= @temperature_threshold do
+      "btn-success"
+    else
+      "btn-danger"
+    end
   end
 end
