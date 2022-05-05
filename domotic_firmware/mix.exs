924899993704bfcc528c23621f9f7800fee5c7a9
Guillaume Cauchon <gcauchon@gmail.com>
2022-05-05 12:46:21 -0400

    Cleanup config

--- domotic_firmware/mix.exs
+++ domotic_firmware/mix.exs
@@ -1,29 +1,22 @@
 defmodule DomoticFirmware.MixProject do
   use Mix.Project
 
+  @app :domotic_firmware
+  @version "0.4.0"
   @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :x86_64]
 
   def project do
     [
-      app: :domotic_firmware,
-      version: "0.4.0",
+      app: @app,
+      version: @version,
       elixir: "~> 1.13",
       archives: [nerves_bootstrap: "~> 1.10"],
       start_permanent: Mix.env() == :prod,
       build_embedded: true,
-      aliases: [loadconfig: [&bootstrap/1]],
       deps: deps(),
-      releases: [{:domotic_firmware, release()}],
+      releases: [{@app, release()}],
       preferred_cli_target: [run: :host, test: :host]
     ]
-  end
-
-  # Starting nerves_bootstrap adds the required aliases to Mix.Project.config()
-  # Aliases are only added if MIX_TARGET is set.
-  def bootstrap(args) do
-    Application.start(:nerves_bootstrap)
-
-    Mix.Task.run("loadconfig", args)
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -38,17 +31,17 @@ defmodule DomoticFirmware.MixProject do
   defp deps do
     [
       # Dependencies for all targets
-      {:nerves, "~> 1.7", runtime: false},
-      {:shoehorn, "~> 0.9"},
-      {:ring_logger, "~> 0.8"},
-      {:toolshed, "~> 0.2"},
+      {:nerves, "~> 1.7.15", runtime: false},
+      {:shoehorn, "~> 0.9.1"},
+      {:ring_logger, "~> 0.8.3"},
+      {:toolshed, "~> 0.2.13"},
 
       # Phoenix web UI
-      {:domotic, path: "../domotic_ui"},
+      {:domotic, path: "../domotic_ui", targets: @all_targets, env: Mix.env()},
 
       # Dependencies for all targets except :host
-      {:nerves_runtime, "~> 0.11", targets: @all_targets},
-      {:nerves_pack, "~> 0.7", targets: @all_targets},
+      {:nerves_runtime, "~> 0.11.6", targets: @all_targets},
+      {:nerves_pack, "~> 0.7.0", targets: @all_targets},
 
       # Dependencies for specific targets
       {:nerves_system_rpi, "~> 1.18", runtime: false, targets: :rpi},
@@ -58,6 +51,7 @@ defmodule DomoticFirmware.MixProject do
       {:nerves_system_rpi3a, "~> 1.18", runtime: false, targets: :rpi3a},
       {:nerves_system_rpi4, "~> 1.18", runtime: false, targets: :rpi4},
       {:nerves_system_bbb, "~> 2.13", runtime: false, targets: :bbb},
+      {:nerves_system_osd32mp1, "~> 0.9", runtime: false, targets: :osd32mp1},
       {:nerves_system_x86_64, "~> 1.18", runtime: false, targets: :x86_64},
 
       # Use GPIOs in Elixir
@@ -68,7 +62,7 @@ defmodule DomoticFirmware.MixProject do
   def release do
     [
       overwrite: true,
-      cookie: "domotic_firmware_cookie",
+      cookie: "{@app}_cookie",
       include_erts: &Nerves.Release.erts/0,
       steps: [&Nerves.Release.init/1, :assemble],
       strip_beams: Mix.env() == :prod
